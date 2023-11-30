5838e15064ade1329305a3244f1f49da8faf6866
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-11-30 22:50:22 +0500

    custom pagination

--- post/views.py
+++ post/views.py
@@ -1,10 +1,15 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from shared.custom_pagination import CustomPagination
 from . models import Post, PostComment, PostLike, CommentLike
 from . serializer import PostSerializer, PostCommentSerializer
 
 
 class PostListAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny,)
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        return Post.objects.all().order_by('-created_at')
---
+++ shared/custom_pagination.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'page_size': self.page_size,
+            'results': data
+        })
