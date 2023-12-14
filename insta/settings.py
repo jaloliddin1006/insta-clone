a6091ec6148ef98fcb6c014a90cc41d0d4d983c6
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-12-14 11:50:51 +0500

    update access limit

--- insta/settings.py
+++ insta/settings.py
@@ -69,8 +69,8 @@ REST_FRAMEWORK = {
 ...
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
